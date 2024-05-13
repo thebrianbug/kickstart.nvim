2d4c37123f3b570dda73722b1504e8d1118c8900
Brian McIlwain <technicallyemployed@gmail.com>
2024-05-13 18:43:57 -0300

    Configure needed file type servers and lints

--- init.lua
+++ init.lua
@@ -573,6 +573,14 @@ require('lazy').setup({
         -- pyright = {},
         -- rust_analyzer = {},
         tsserver = {},
+        svelte = {},
+        cssls = {},
+        html = {},
+        jsonls = {},
+        prismals = {},
+        dockerls = {},
+        docker_compose_language_service = {},
+        tailwindcss = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -667,6 +675,7 @@ require('lazy').setup({
         typescript = { { 'prettier' } },
         json = { { 'prettier' } },
         css = { { 'prettier' } },
+        toml = { { 'prettier' } },
       },
     },
   },
