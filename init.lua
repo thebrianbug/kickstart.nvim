4ef0688a0c1165c22a4989074bafd76125e091b2
Brian McIlwain <technicallyemployed@gmail.com>
2024-05-13 15:39:22 -0300

    Added vim fugitive

--- init.lua
+++ init.lua
@@ -884,6 +884,7 @@ require('lazy').setup({
       require('leap').create_default_mappings()
     end,
   },
+  { 'tpope/vim-fugitive' },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
