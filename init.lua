1b44fabcde9627f895a689c907227714ddb7ed60
Brian McIlwain <technicallyemployed@gmail.com>
2024-05-14 10:56:12 -0300

    Removed broken plugin

--- init.lua
+++ init.lua
@@ -831,17 +831,6 @@ require('lazy').setup({
       },
     },
   },
-  {
-    'nvim-neotest/neotest',
-    dependencies = {
-      'nvim-neotest/nvim-nio',
-      'nvim-lua/plenary.nvim',
-      'antoinemadec/FixCursorHold.nvim',
-      'nvim-treesitter/nvim-treesitter',
-      'marilari88/neotest-vitest',
-      'thenbe/neotest-playwright',
-    },
-  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
