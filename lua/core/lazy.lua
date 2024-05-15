698c84cdcf79c8cee0f75c6e24762c92af6ee48a
Brian McIlwain <technicallyemployed@gmail.com>
2024-05-15 11:30:14 -0300

    Add gh.nvim for PRs

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -24,10 +24,10 @@ require('lazy').setup {
     'folke/which-key.nvim',
     event = 'VimEnter',
     config = function()
-      require('which-key').setup()
+      local wk = require 'which-key'
+      wk.setup()
 
-      -- Document existing key chains
-      require('which-key').register {
+      wk.register {
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
         ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
@@ -36,8 +36,59 @@ require('lazy').setup {
         ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
         ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
       }
-      -- visual mode
-      require('which-key').register({
+
+      wk.register({
+        g = {
+          name = '+Git',
+          h = {
+            name = '+Github',
+            c = {
+              name = '+Commits',
+              c = { '<cmd>GHCloseCommit<cr>', 'Close' },
+              e = { '<cmd>GHExpandCommit<cr>', 'Expand' },
+              o = { '<cmd>GHOpenToCommit<cr>', 'Open To' },
+              p = { '<cmd>GHPopOutCommit<cr>', 'Pop Out' },
+              z = { '<cmd>GHCollapseCommit<cr>', 'Collapse' },
+            },
+            i = {
+              name = '+Issues',
+              p = { '<cmd>GHPreviewIssue<cr>', 'Preview' },
+            },
+            l = {
+              name = '+Litee',
+              t = { '<cmd>LTPanel<cr>', 'Toggle Panel' },
+            },
+            r = {
+              name = '+Review',
+              b = { '<cmd>GHStartReview<cr>', 'Begin' },
+              c = { '<cmd>GHCloseReview<cr>', 'Close' },
+              d = { '<cmd>GHDeleteReview<cr>', 'Delete' },
+              e = { '<cmd>GHExpandReview<cr>', 'Expand' },
+              s = { '<cmd>GHSubmitReview<cr>', 'Submit' },
+              z = { '<cmd>GHCollapseReview<cr>', 'Collapse' },
+            },
+            p = {
+              name = '+Pull Request',
+              c = { '<cmd>GHClosePR<cr>', 'Close' },
+              d = { '<cmd>GHPRDetails<cr>', 'Details' },
+              e = { '<cmd>GHExpandPR<cr>', 'Expand' },
+              o = { '<cmd>GHOpenPR<cr>', 'Open' },
+              p = { '<cmd>GHPopOutPR<cr>', 'PopOut' },
+              r = { '<cmd>GHRefreshPR<cr>', 'Refresh' },
+              t = { '<cmd>GHOpenToPR<cr>', 'Open To' },
+              z = { '<cmd>GHCollapsePR<cr>', 'Collapse' },
+            },
+            t = {
+              name = '+Threads',
+              c = { '<cmd>GHCreateThread<cr>', 'Create' },
+              n = { '<cmd>GHNextThread<cr>', 'Next' },
+              t = { '<cmd>GHToggleThread<cr>', 'Toggle' },
+            },
+          },
+        },
+      }, { prefix = '<leader>' })
+
+      wk.register({
         ['<leader>h'] = { 'Git [H]unk' },
       }, { mode = 'v' })
     end,
@@ -445,6 +496,21 @@ require('lazy').setup {
         },
       },
     },
+  },
+
+  {
+    'ldelossa/gh.nvim',
+    dependencies = {
+      {
+        'ldelossa/litee.nvim',
+        config = function()
+          require('litee.lib').setup()
+        end,
+      },
+    },
+    config = function()
+      require('litee.gh').setup()
+    end,
   },
 
   -- require 'kickstart.plugins.debug',
