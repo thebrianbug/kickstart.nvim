169e4566480eb4e2b760f8729631118b5a2622b0
Brian McIlwain <technicallyemployed@gmail.com>
2024-05-15 13:06:10 -0300

    Use Octo.nvim for GitHub

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -38,57 +38,6 @@ require('lazy').setup {
       }
 
       wk.register({
-        g = {
-          name = '+Git',
-          h = {
-            name = '+Github',
-            c = {
-              name = '+Commits',
-              c = { '<cmd>GHCloseCommit<cr>', 'Close' },
-              e = { '<cmd>GHExpandCommit<cr>', 'Expand' },
-              o = { '<cmd>GHOpenToCommit<cr>', 'Open To' },
-              p = { '<cmd>GHPopOutCommit<cr>', 'Pop Out' },
-              z = { '<cmd>GHCollapseCommit<cr>', 'Collapse' },
-            },
-            i = {
-              name = '+Issues',
-              p = { '<cmd>GHPreviewIssue<cr>', 'Preview' },
-            },
-            l = {
-              name = '+Litee',
-              t = { '<cmd>LTPanel<cr>', 'Toggle Panel' },
-            },
-            r = {
-              name = '+Review',
-              b = { '<cmd>GHStartReview<cr>', 'Begin' },
-              c = { '<cmd>GHCloseReview<cr>', 'Close' },
-              d = { '<cmd>GHDeleteReview<cr>', 'Delete' },
-              e = { '<cmd>GHExpandReview<cr>', 'Expand' },
-              s = { '<cmd>GHSubmitReview<cr>', 'Submit' },
-              z = { '<cmd>GHCollapseReview<cr>', 'Collapse' },
-            },
-            p = {
-              name = '+Pull Request',
-              c = { '<cmd>GHClosePR<cr>', 'Close' },
-              d = { '<cmd>GHPRDetails<cr>', 'Details' },
-              e = { '<cmd>GHExpandPR<cr>', 'Expand' },
-              o = { '<cmd>GHOpenPR<cr>', 'Open' },
-              p = { '<cmd>GHPopOutPR<cr>', 'PopOut' },
-              r = { '<cmd>GHRefreshPR<cr>', 'Refresh' },
-              t = { '<cmd>GHOpenToPR<cr>', 'Open To' },
-              z = { '<cmd>GHCollapsePR<cr>', 'Collapse' },
-            },
-            t = {
-              name = '+Threads',
-              c = { '<cmd>GHCreateThread<cr>', 'Create' },
-              n = { '<cmd>GHNextThread<cr>', 'Next' },
-              t = { '<cmd>GHToggleThread<cr>', 'Toggle' },
-            },
-          },
-        },
-      }, { prefix = '<leader>' })
-
-      wk.register({
         ['<leader>h'] = { 'Git [H]unk' },
       }, { mode = 'v' })
     end,
@@ -109,7 +58,7 @@ require('lazy').setup {
         end,
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      { 'nvim-tree/nvim-web-devicons' },
     },
     config = function()
       require('telescope').setup {
@@ -499,17 +448,20 @@ require('lazy').setup {
   },
 
   {
-    'ldelossa/gh.nvim',
-    dependencies = {
-      {
-        'ldelossa/litee.nvim',
-        config = function()
-          require('litee.lib').setup()
-        end,
-      },
+    'pwntester/octo.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'nvim-tree/nvim-web-devicons',
     },
     config = function()
-      require('litee.gh').setup()
+      require('octo').setup()
+
+      vim.keymap.set('n', '<leader>ghi', '<CMD>Octo issue list<CR>', { desc = '[G]it[H]ub [I]ssue List' })
+      vim.keymap.set('n', '<leader>ghp', '<CMD>Octo pr list<CR>', { desc = '[G]it[H]ub [P]r List' })
+
+      vim.keymap.set('i', '@', '@<C-x><C-o>', { silent = true, buffer = true, desc = 'Enable completion for GitHub usernames' })
+      vim.keymap.set('i', '#', '#<C-x><C-o>', { silent = true, buffer = true, desc = 'Enable completion for GitHub issues' })
     end,
   },
 
