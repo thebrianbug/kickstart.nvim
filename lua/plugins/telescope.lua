0ef647e8053f8c5865446d0f643884ce82e1f4ae
Brian McIlwain <technicallyemployed@gmail.com>
2024-05-16 14:54:36 -0300

    Telescope use grep with args

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -35,6 +35,7 @@ return {
       pcall(telescope.load_extension, 'live_grep_args')
 
       local builtin = require 'telescope.builtin'
+      local live_grep_args = require('telescope').extensions.live_grep_args
       local set = vim.keymap.set
 
       set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
@@ -42,7 +43,7 @@ return {
       set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      set('n', '<leader>sg', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = '[S]earch by [G]rep' })
+      set('n', '<leader>sg', live_grep_args.live_grep_args, { desc = '[S]earch by [G]rep' })
       set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
